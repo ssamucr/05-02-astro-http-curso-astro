d0dcf18ac37ba1f9568db1867b1db1d6b99961f3
ssamucr <ssamucr@gmail.com>
2026-04-04 12:35:22 -0500

    Added cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+
+  // output: 'static',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
   integrations: [mdx(), sitemap()],
-  output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+
+  adapter: cloudflare(),
 });
